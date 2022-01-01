3c25f41e9f09731ad33b46d627ae014b6fedc67c
Anwesh43 <anweshthecool0@gmail.com>
2022-01-02 00:36:07 +0530

    adding closure

--- functions_demo.ex
+++ functions_demo.ex
@@ -1,3 +1,8 @@
 add = fn a, op -> a + op.(a) end 
 cube = fn a -> a * a * a end
 IO.inspect add.(10, cube)
+
+addMore = fn a -> fn b -> a + b end end 
+addMore10 = addMore.(10)
+result = addMore10.(200)
+IO.puts("Result is #{result}")
