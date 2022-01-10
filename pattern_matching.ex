e3653a5d2506a28dc08e79e43b8ada089ab4bd4b
Anwesh43 <anweshthecool0@gmail.com>
2022-01-10 23:50:48 +0530

    adding pattern matching

---
+++ pattern_matching.ex
@@ -0,0 +1,10 @@
+"Hello " <> command = "Hello World"
+IO.puts command
+
+{a, b, c} = {10, 20, 30}
+IO.puts a 
+IO.puts b 
+IO.puts c 
+
+"eoD " <> last_name = String.reverse("John Doe")
+IO.puts String.reverse(last_name)
