8f407ea782c82d87ccf7892cd28eb510cb25e439
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 23:23:42 +0530

    adding array_util demo

--- array_util.ex
+++ array_util.ex
@@ -2,7 +2,19 @@ defmodule ArrayUtil do
     def add(arr, elem), do: [elem | arr]
     def addElem(0), do: []  
     def addElem(n), do: add(addElem(n - 1), n)
-    def split(arr), do: Enum.split(arr, div(Enum.count(arr), 2))
+    def split([]), do: []
+    def split([a]), do: [a]
+    defp merge(list1 = [], list2), do: list2 
+    defp merge(list1, list2 = []), do: list1
+    defp merge(list1 = [head1|tail1], list2 = [head2|tail2]) when head1 < head2, do: [head1 | merge(tail1, list2)]
+    defp merge(list1 = [head1|tail1], list2 = [head2|tail2]) when head2 < head1, do: [head2 | merge(list1, tail2)]
+    def split(arr), do: Enum.split(arr, div(Enum.count(arr), 2)) 
+    def sort([]), do: []
+    def sort([a]), do: [a]
+    def sort(arr) do 
+        {list1, list2} = split(arr)
+        arr = merge(sort(list1), sort(list2))
+    end 
 end
 
 
@@ -17,3 +29,5 @@ IO.puts "First array"
 IO.inspect a 
 IO.puts "Second array"
 IO.inspect b 
+IO.puts "sorted array"
+IO.inspect ArrayUtil.sort([3, 1, 5, 7, 6, 9, 8, 4, 2, 13, 12, 11, 10])
