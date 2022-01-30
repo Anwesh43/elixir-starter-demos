d61665bad93e67644676238bef80c69353528292
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 18:32:50 +0530

    demo for transforming an array of keyvalue pairs

---
+++ enchanter_shop.ex
@@ -0,0 +1,25 @@
+defmodule EnchanterShop do 
+    @name  "Sachin"
+    def test_data do 
+        [
+            %{name: "Gloves", price: 1000, sellable: true},
+            %{name: "Pads", price: 1000, sellable: true},
+            %{name: "Bat", price: 2000, sellable: true},
+            %{name: "Helmet", price: 1500, sellable: true}
+        ]
+    end
+    def transform([]), do: []
+    def transform([first_item|rest]) do 
+        new_item = %{
+            name: "#{@name}'s #{first_item.name}",
+            price: "Rs. #{first_item.price}",
+            sellable: first_item.sellable
+        }
+        [
+            new_item | transform(rest)
+        ]
+    end
+end 
+
+data = EnchanterShop.test_data()
+IO.inspect EnchanterShop.transform(data)
