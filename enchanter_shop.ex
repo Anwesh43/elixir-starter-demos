d91001b81d0c4f9258807cf635c7b0a8fbbf633e
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 22:22:52 +0530

    filtering out values

--- enchanter_shop.ex
+++ enchanter_shop.ex
@@ -5,10 +5,14 @@ defmodule EnchanterShop do
             %{name: "Gloves", price: 1000, sellable: true},
             %{name: "Pads", price: 1000, sellable: true},
             %{name: "Bat", price: 2000, sellable: true},
-            %{name: "Helmet", price: 1500, sellable: true}
+            %{name: "Helmet", price: 1500, sellable: true},
+            %{name: "Ball", price: 300, sellable: false}
         ]
     end
     def transform([]), do: []
+    def transform([first_item=%{sellable: false}|next_items]) do     
+        [first_item | transform(next_items)]
+    end
     def transform([first_item|rest]) do 
         new_item = %{
             name: "#{@name}'s #{first_item.name}",
