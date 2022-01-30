0489eebc209d65225a6d659ed14376f449ed9cd9
Anwesh43 <anweshthecool0@gmail.com>
2022-01-30 23:07:22 +0530

    splitting an array

--- array_util.ex
+++ array_util.ex
@@ -2,10 +2,18 @@ defmodule ArrayUtil do
     def add(arr, elem), do: [elem | arr]
     def addElem(0), do: []  
     def addElem(n), do: add(addElem(n - 1), n)
+    def split(arr), do: Enum.split(arr, div(Enum.count(arr), 2))
 end
+
 
 arr = ArrayUtil.add([], 1)
 arr = ArrayUtil.add(arr, 2)
 arr = ArrayUtil.add(arr, 5)
 IO.inspect arr 
 IO.inspect ArrayUtil.addElem(10)
+arr = ArrayUtil.addElem(10)
+{a, b} = ArrayUtil.split(arr)
+IO.puts "First array"
+IO.inspect a 
+IO.puts "Second array"
+IO.inspect b 
